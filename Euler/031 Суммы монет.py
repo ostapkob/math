fa0b6e66d71877453ede3c4f0fa7cbc4f0a32f46
ostapkob <ostap666@yandex.ru>
2019-07-19 11:51:19 +1000

    31 not correct

--- Euler/031 Суммы монет.py
+++ Euler/031 Суммы монет.py
@@ -1,16 +1,13 @@
+#This task not correct, it solve count permutations all coin
 from functools import lru_cache
 
-# @lru_cache()
-def kuzn(n, ls):
-
+@lru_cache()
+def kuzn(n):
+    global coin
     if n <= 1:
         return 1
     else:
-        # new_ls = [x for x in ls if x<=n]
-        return sum([kuzn(n-x, ls) for x in [x for x in ls if x<=n]])
+        return sum([kuzn(n-x) for x in [x for x in coin if x<=n]])
 
-# coin = [1, 2, 5, ]
-coin = [1,2,5,10]
-print(kuzn(27, coin))
-# for i in range(12):
-#     print(i, ':', kuzn(i, coin))
+coin = [1,2,5,10,20, 50, 100, 200]
+print(kuzn(200))
